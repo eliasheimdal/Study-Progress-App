de8ea2d9e5b7005196366cc599d7584ef3cbaa6f
Elias <eliaswhe@stud.ntnu.no>
2025-02-09 11:17:27 +0100

    Changed layout of progress/homepage

--- components/lectureTracker.tsx
+++ components/lectureTracker.tsx
@@ -1,7 +1,7 @@
-"use client"
+"use client";
 
 import React, { useState, useEffect } from "react";
-import { Checkbox, CheckboxGroup, Button } from "@heroui/react";
+import { Checkbox, CheckboxGroup, Button, Card } from "@heroui/react";
 import ProgressBar from "@/components/progress";
 import SliderLoad from "@/components/slider";
 import ActivityForm from "@/components/activityForm";
@@ -12,42 +12,48 @@ import { Activity } from "@/types";
 import { useSession } from "next-auth/react";
 import { motion } from "framer-motion";
 
-export default function LectureTracker({ lectures, courses }: { lectures: Lecture[]; courses: Course[] }) {
+export default function LectureTracker({
+  lectures,
+  courses,
+}: {
+  lectures: Lecture[];
+  courses: Course[];
+}) {
   const { data: session } = useSession();
 
-  
   const loadProgress = () => {
     if (typeof window !== "undefined") {
       return JSON.parse(localStorage.getItem("progress") || "{}");
     }
     return {};
   };
-  
+
   const loadActivities = () => {
     if (typeof window !== "undefined") {
       return JSON.parse(localStorage.getItem("activities") || "[]");
     }
     return [];
   };
-  
-  
+
   const [progress, setProgress] = useState(loadProgress);
   const [workload, setWorkload] = useState(9.375);
-  
-  
+
   const calculateProgressPercent = (progress: { [key: string]: number }) => {
-    return Object.keys(progress).reduce((acc, courseCode) => {
-      acc[courseCode] = (progress[courseCode] / workload) * 100;
-      return acc;
-    }, {} as { [key: string]: number });
+    return Object.keys(progress).reduce(
+      (acc, courseCode) => {
+        acc[courseCode] = (progress[courseCode] / workload) * 100;
+        return acc;
+      },
+      {} as { [key: string]: number }
+    );
   };
-  
-  
-  const [progressPercent, setProgressPercent] = useState(() => calculateProgressPercent(loadProgress()));
+
+  const [progressPercent, setProgressPercent] = useState(() =>
+    calculateProgressPercent(loadProgress())
+  );
   const [activities, setActivities] = useState(loadActivities);
   const [selected, setSelected] = useState<string[]>([]);
 
-  
   useEffect(() => {
     localStorage.setItem("progress", JSON.stringify(progress));
     localStorage.setItem("activities", JSON.stringify(activities));
@@ -56,13 +62,13 @@ export default function LectureTracker({ lectures, courses }: { lectures: Lectur
   const updateProgress = (time: number, courseCode: string) => {
     setProgress((prev: Record<string, number>) => {
       const updatedProgress: Record<string, number> = {
-      ...prev,
-      [courseCode]: (prev[courseCode] || 0) + time,
+        ...prev,
+        [courseCode]: (prev[courseCode] || 0) + time,
       };
 
       setProgressPercent((prevPercent: Record<string, number>) => ({
-      ...prevPercent,
-      [courseCode]: (updatedProgress[courseCode] / workload) * 100,
+        ...prevPercent,
+        [courseCode]: (updatedProgress[courseCode] / workload) * 100,
       }));
 
       return updatedProgress;
@@ -71,15 +77,19 @@ export default function LectureTracker({ lectures, courses }: { lectures: Lectur
 
   const handleActivitySubmit = (activity: Activity) => {
     const duration = parseInt(activity.duration, 10);
-    
-    
+
     setActivities((prev: Activity[]) => {
-      if (prev.some((act: Activity) => act.course === activity.course && act.duration === activity.duration)) {
-      return prev; 
+      if (
+        prev.some(
+          (act: Activity) =>
+            act.course === activity.course && act.duration === activity.duration
+        )
+      ) {
+        return prev;
       }
       return [...prev, activity];
     });
-  
+
     updateProgress(duration, activity.course);
   };
 
@@ -89,19 +99,21 @@ export default function LectureTracker({ lectures, courses }: { lectures: Lectur
 
     setProgress((prev: Record<string, number>) => {
       const updatedProgress = {
-      ...prev,
-      [courseCode]: (prev[courseCode] || 0) - duration,
+        ...prev,
+        [courseCode]: (prev[courseCode] || 0) - duration,
       };
 
       setProgressPercent((prevPercent: Record<string, number>) => ({
-      ...prevPercent,
-      [courseCode]: (updatedProgress[courseCode] / workload) * 100,
+        ...prevPercent,
+        [courseCode]: (updatedProgress[courseCode] / workload) * 100,
       }));
 
       return updatedProgress;
     });
 
-    setActivities((prev: Activity[]) => prev.filter((_, i: number) => i !== index));
+    setActivities((prev: Activity[]) =>
+      prev.filter((_, i: number) => i !== index)
+    );
   };
 
   const handleReset = () => {
@@ -115,78 +127,85 @@ export default function LectureTracker({ lectures, courses }: { lectures: Lectur
 
   return (
     <motion.div
-    initial={{ opacity: 0 }}
-    animate={{ opacity: 1 }}
-    exit={{ opacity: 0 }}
-    transition={{ duration: 1.3 }}
-  >
-    <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-      {session ? (
-        <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
-          <p>Welcome, {session.user?.name}!</p>
-          <h1 className={`${title({ color: "blue" })} pb-2`}>Progress</h1>
-
-          {courses.map((course) => (
-            <ProgressBar
-              key={course.courseCode}
-              name={course.name}
-              code={course.courseCode}
-              value={progressPercent[course.courseCode] || 0}
-              effort={progress[course.courseCode] || 0}
-              full={workload}
-            />
-          ))}
-
-          <CheckboxGroup
-            label="Select Attended Lectures"
-            value={selected}
-            onValueChange={setSelected}
-          >
-            {lectures.map((lecture) => (
-              <Checkbox
-                key={lecture.id}
-                value={lecture.id.toString()}
-                onChange={(e) =>
-                  updateProgress(
-                    e.target.checked ? lecture.durationHours : -lecture.durationHours,
-                    lecture.courseCode
-                  )
-                }
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      transition={{ duration: 1.3 }}
+    >
+      <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+        {session ? (
+          <div className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
+            <p>Welcome, {session.user?.name}!</p>
+            <h1 className={`${title({ color: "blue" })} pb-2`}>Progress</h1>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6 border-t border-grey pt-4">
+            <Card className="w-full p-8 border shadow-lg">
+              <CheckboxGroup
+                label="Select Attended Lectures"
+                value={selected}
+                onValueChange={setSelected}
               >
-                {`${lecture.courseCode} - ${lecture.type} (${lecture.day} ${lecture.time})`}
-              </Checkbox>
-            ))}
-          </CheckboxGroup>
+                {lectures.map((lecture) => (
+                  <Checkbox
+                    key={lecture.id}
+                    value={lecture.id.toString()}
+                    onChange={(e) =>
+                      updateProgress(
+                        e.target.checked
+                          ? lecture.durationHours
+                          : -lecture.durationHours,
+                        lecture.courseCode
+                      )
+                    }
+                  >
+                    {`${lecture.courseCode} - ${lecture.type} (${lecture.day} ${lecture.time})`}
+                  </Checkbox>
+                ))}
+              </CheckboxGroup>
+            </Card>
+              <Card className="w-full p-8 border shadow-lg">
+                {courses.map((course) => (
+                  <ProgressBar
+                    key={course.courseCode}
+                    name={course.name}
+                    code={course.courseCode}
+                    value={progressPercent[course.courseCode] || 0}
+                    effort={progress[course.courseCode] || 0}
+                    full={workload}
+                  />
+                ))}
+              </Card>
+              <Card className="w-full p-8 border shadow-lg">
+              <ActivityForm
+                activities={activities}
+                setActivities={setActivities}
+                onActivitySubmit={handleActivitySubmit}
+                deleteCourse={handleActivityDelete}
+              />
+              </Card>
+            </div>
 
-          <ActivityForm
-            activities={activities}
-            setActivities={setActivities}
-            onActivitySubmit={handleActivitySubmit}
-            deleteCourse={handleActivityDelete}
-          />
+            <h2 className={`${subtitle()} text-center`}>Workload</h2>
+            <SliderLoad
+              onChange={(value: number) => setWorkload((value / 100) * 9.375)}
+              name="Prosent Student"
+              value={100}
+            />
 
-          <h2 className={`${subtitle()} text-center`}>Workload</h2>
-          <SliderLoad
-            onChange={(value: number) => setWorkload((value / 100) * 9.375)}
-            name="Prosent Student"
-            value={100}
-          />
-
-          <div className="flex gap-3">
-            <Button
-              className={buttonStyles({ color: "danger", radius: "full" })}
-              onPress={handleReset}
-            >
-              Reset
-            </Button>
+            <div className="flex gap-3">
+              <Button
+                className={buttonStyles({ color: "danger", radius: "full" })}
+                onPress={handleReset}
+              >
+                Reset
+              </Button>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div>
-          <p>Please login to view your progress</p>
-        </div>
-      )}
-    </div>
+        ) : (
+          <div>
+            <p>Please login to view your progress</p>
+          </div>
+        )}
+      </div>
     </motion.div>
   );
 }
