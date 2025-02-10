ca51abba19898f0d2b266de095024876d08ead76
Elias <eliaswhe@stud.ntnu.no>
2025-02-10 18:59:50 +0100

    remove avatar when not logged in

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -134,25 +134,27 @@ export const Navbar = ({ user }: NavbarProp) => {
           ))}
         </div>
       </NavbarMenu>
-      <Dropdown placement="bottom-end">
-        <DropdownTrigger>
-          <Avatar
-            isBordered
-            as="button"
-            className="transition-transform"
-            color="secondary"
-            name={user?.name ?? "User"}
-            size="sm"
-            src={user?.image ?? ""}
-          />
-        </DropdownTrigger>
-        <DropdownMenu aria-label="Profile Actions" variant="flat">
-          <DropdownItem key="profile" className="h-14 gap-2">
-            <p className="font-semibold">Signed in as</p>
-            <p className="font-semibold">{user?.email}</p>
-          </DropdownItem>
-        </DropdownMenu>
-      </Dropdown>
+      {session ? (
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name={user?.name ?? "User"}
+              size="sm"
+              src={user?.image ?? ""}
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">{user?.email}</p>
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      ) : null}
     </HeroUINavbar>
   );
 };
